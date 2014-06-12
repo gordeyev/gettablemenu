7f839f077a28fe048f40776494358953a43c86e2
Mikhail <m.gordeyev@gmail.com>
2014-06-12 21:26:54 +0400

    prod settings

--- gettablemenu/settings/prod.py
+++ gettablemenu/settings/prod.py
@@ -1,13 +1,22 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gettablemenu',
+        'USER': 'mikhail',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 DATABASES['default'] = dj_database_url.config()
