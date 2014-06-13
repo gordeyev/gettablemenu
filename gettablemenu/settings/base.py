eeed171e30b9cc8b37441802e7068d40ebdaf205
Mikhail <m.gordeyev@gmail.com>
2014-06-13 20:18:44 +0400

    staticfiles for prod

--- gettablemenu/settings/base.py
+++ gettablemenu/settings/base.py
@@ -10,6 +10,21 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from unipath import Path
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+PROJECT_DIR = Path(__file__).ancestor(3)
+
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    PROJECT_DIR.child("static"),
+)
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -60,7 +75,4 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
