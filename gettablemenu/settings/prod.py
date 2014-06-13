3671f5997bf1b7a9fc96431736aea00b0083f1b6
Mikhail <m.gordeyev@gmail.com>
2014-06-13 18:44:41 +0400

    prod config for DATABASE

--- gettablemenu/settings/prod.py
+++ gettablemenu/settings/prod.py
@@ -1,8 +1,6 @@
 from .base import *
 
 import dj_database_url
-from unipath import Path
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -13,26 +11,7 @@ TEMPLATE_DEBUG = True
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gettablemenu',
-        'USER': 'mikhail',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config()
 }
 
-DATABASES['default'] = dj_database_url.config()
-
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'gettablemenu.herokuapp.com']
-
-PROJECT_DIR = Path(__file__).ancestor(3)
-MEDIA_ROOT = PROJECT_DIR.child("media")
-STATIC_ROOT = PROJECT_DIR.child("static")
-STATICFILES_DIRS = (
-	PROJECT_DIR.child("assets"),
-)
-# TEMPLATE_DIRS = (
-# ￼￼￼￼￼￼￼    PROJECT_DIR.child("templates"),
-# )
