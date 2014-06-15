19eb8318b88a5b8d6403a41f6d8f894338c39a0f
Mikhail <m.gordeyev@gmail.com>
2014-06-15 08:41:25 +0400

    New models Resto, RestMenu, Food

--- menu/models.py
+++ menu/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-import mptt
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class MenuRuURLs(models.Model):
@@ -9,3 +9,34 @@ class MenuRuURLs(models.Model):
 
 	def __unicode__(self):
 		return self.title
+
+
+class Food(models.Model):
+	name = models.CharField(max_length=200)
+	weight = models.FloatField(null=True)
+	cost = models.FloatField(null=True)
+
+	def __unicode__(self):
+		return self.name
+
+
+class RestoMenu(MPTTModel):
+	level = models.IntegerField()
+	name = models.CharField(max_length=200)
+	food = models.FloatField(Food)
+	parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+
+	def __unicode__(self):
+		return self.name
+
+
+class Resto(models.Model):
+	name = models.CharField(max_length=200)
+	url = models.URLField(max_length=200)
+	gettable_id = models.IntegerField(null=True)
+	x = models.FloatField()
+	y = models.FloatField()
+	menu = models.ForeignKey(RestoMenu)
+
+	def __unicode__(self):
+		return self.name
